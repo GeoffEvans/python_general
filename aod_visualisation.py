570b0af95c5aafefeff6b545552ade9c2d63e6c7
geoff22873 <geoffrey.evans.13@ucl.ac.uk>
2014-06-26 18:29:19 +0100

    added more aod graphs

--- aod_visualisation.py
+++ aod_visualisation.py
@@ -3,20 +3,21 @@ from ray import Ray
 from acoustics import Acoustics
 from numpy import linspace, pi, sin, cos, abs, sqrt, arcsin
 from xu_stroud_model import diffract_by_wavevector_triangle
+from vector_utils import normalise
 
-normal = [0,0,1]
+normal = normalise([0,0,100])
 sound_direction = [1,0,0]
-aperture_width = 15e-3
-transducer_width = 3.6e-3
-crystal_thickness = 5e-3
+aperture_width = 25e-3
+transducer_width = 1.2e-3
+crystal_thickness = 8e-3
 order = -1
 op_wavelength_vac = 800e-9
-resolution = 40
+resolution = 100
 
 aod = Aod(normal, sound_direction, aperture_width, transducer_width, crystal_thickness)
 
-mhz_range = linspace(20, 60, resolution)
-degrees_range =  linspace(0, 4, resolution) 
+mhz_range = linspace(10, 60, resolution)
+degrees_range =  linspace(-1, 6, resolution) 
 
 def generic_plot_surface(x_array, y_array, z_func, labels):
     from mpl_toolkits.mplot3d import Axes3D
@@ -32,11 +33,18 @@ def generic_plot_surface(x_array, y_array, z_func, labels):
     ax = fig.gca()               
     cs = ax.pcolor(x, y, z)   
     
+    def onclick(event):
+        print 'button=%d, x=%d, y=%d, xdata=%f, ydata=%f'%(
+            event.button, event.x, event.y, event.xdata, event.ydata)
+
+    cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    
     ax.set_xlabel(labels[0])
     ax.set_ylabel(labels[1])
-    cb = plt.colorbar(cs, orientation = 'horizontal') 
+    cb = plt.colorbar(cs, orientation = 'vertical') 
     cb.set_label(labels[2]) 
- 
+
+    plt.grid() 
     plt.show()
 
 def plot_mismatch_angle_freq():
@@ -72,8 +80,7 @@ def plot_efficiency_xangle_yangle():
     def func(deg, deg_trans):
         ang = deg * pi/180
         ang_trans = deg_trans * pi/180
-        wavevector_unit = [sin(ang), sin(ang_trans), sqrt(1 - sin(ang)**2 - sin(ang_trans)**2)]
-        import pdb; pdb.set_trace
+        wavevector_unit = [ang, ang_trans, sqrt(1 - ang**2 - ang_trans**2)]
         ray = Ray([0,0,0], wavevector_unit, op_wavelength_vac)
         acoustics = Acoustics(35e6, 1)        
         
@@ -81,7 +88,7 @@ def plot_efficiency_xangle_yangle():
         return ray.energy
     
     labels = ["incidence angle / deg","transverse incidence angle / deg","efficiency"]
-    generic_plot_surface(degrees_range, degrees_range, func, labels)
+    generic_plot_surface(linspace(0.03, 0.08, 20)/pi*180, linspace(-0.3, 0.3, 60)/pi*180, func, labels)
 
 def plot_xangleout_xangle_freq():
     
@@ -110,4 +117,54 @@ def plot_xangleout_xangle_yangle():
         return arcsin(ray.wavevector_unit[0]) * 180/pi
     
     labels = ["incidence angle / deg","transverse incidence angle / deg","diffracted angle / deg"]
-    generic_plot_surface(degrees_range, degrees_range, func, labels)
+    generic_plot_surface(degrees_range, linspace(-5, 5, resolution), func, labels)
+
+def generic_plot(x, y_func, labels):
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+    from matplotlib import cm
+    from numpy import meshgrid, vectorize
+    
+    y_func_vec = vectorize(y_func)
+    y = y_func_vec(x) 
+    
+    fig = plt.figure()
+    ax = fig.gca()               
+    cs = ax.plot(x, y)   
+    
+    ax.set_xlabel(labels[0])
+    ax.set_ylabel(labels[1])
+ 
+    plt.grid()
+    plt.show()
+
+def plot_efficiency_freq():
+    
+    def func(mhz):
+        ang = 2.2 * pi / 180
+        wavevector_unit = [sin(ang), 0, cos(ang)]
+        ray = Ray([0,0,0], wavevector_unit, op_wavelength_vac)
+        acoustics = Acoustics(mhz*1e6, 1)
+        
+        aod.propagate_ray([ray], [acoustics], order)
+        return ray.energy
+    
+    labels = ["frequency / MHz","efficiency"]
+    generic_plot(mhz_range, func, labels)
+    
+def plot_efficiency_xangle():
+    
+    def func(deg):
+        ang = deg * pi/180
+        wavevector_unit = [sin(ang), 0, cos(ang)]
+        ray = Ray([0,0,0], wavevector_unit, op_wavelength_vac)
+        acoustics = Acoustics(35e6, 1.5)
+        
+        aod.propagate_ray([ray], [acoustics], order)
+        return ray.energy
+    
+    labels = ["incidence angle / deg","efficiency"]
+    generic_plot(degrees_range, func, labels)
+    
+if __name__ == '__main__':
+    plot_efficiency_xangle()
