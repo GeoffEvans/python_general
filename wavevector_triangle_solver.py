63c5dac5308cd1d074a358466910d05773eb57b0
geoff22873 <geoffrey.evans.13@ucl.ac.uk>
2015-01-07 12:01:43 +0000

    removed nonesense from wavevector triangle solver

--- wavevector_triangle_solver.py
+++ wavevector_triangle_solver.py
@@ -1,5 +1,5 @@
 from numpy.linalg import norm
-from numpy import dot, outer, zeros, abs, all, power, sum
+from numpy import dot, outer, zeros, abs, all, sum
 from scipy.optimize import fsolve
 from vector_utils import normalise_list
 
@@ -32,34 +32,6 @@ def method1(sum_vector, base_length, normal, multiplier_func):
         if all(abs(ratio - 1) < 1e-14):
             return sv
         new_sum_vector = sv - outer(dot(sv, normal) * (1 - ratio), normal)
-        return tail_rec_solve(new_sum_vector)
-        
-    wavevec_out = tail_rec_solve(sum_vector)
-    wavevectors_out_unit = normalise_list(wavevec_out)
-    
-    wavevector_mismatches_mag = dot(wavevec_out - sum_vector, normal)    
-    
-    return (wavevector_mismatches_mag, wavevectors_out_unit, base_length)
-    
-def method2(sum_vector, base_length, normal, multiplier_func): #not working
-    
-    def tail_rec_solve(sv):
-        multiplier = multiplier_func(sv)
-        
-        ratio = base_length * multiplier / norm(sv, axis=1)
-        if all(abs(ratio - 1) < 1e-14):
-            return sv
-            
-        dir = normalise_list(sv)
-        new_dir = dir + normal * 1e-12
-        new_multiplier = multiplier_func(new_dir)
-
-        k = (multiplier * dir.T * base_length).T
-        new_k = (new_multiplier * new_dir.T * base_length).T
-        tangent = normalise_list(new_k - k)
-        
-        shift = (dot(sv, normal) - dot_(sv, tangent) * dot(tangent, normal)) / (1 - power(dot(tangent, normal), 2.))
-        new_sum_vector = sv + outer(shift, normal)
         return tail_rec_solve(new_sum_vector)
         
     wavevec_out = tail_rec_solve(sum_vector)
