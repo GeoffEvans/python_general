4d56d603f5b4d2c83d8924af3607cc885882a2da
Geoff Evans <geoffrey.evans.13@ucl.ac.uk>
2015-03-24 17:49:13 +0000

    simplified experimental plotting

--- aol_modelling/aol_performance/aod_model_expt_comparison.py
+++ aol_modelling/aol_performance/aod_model_expt_comparison.py
@@ -2,6 +2,10 @@ from aol_model.aod_visualisation import AodVisualisation
 from aol_model.vector_utils import normalise
 import expt_data as d
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+
+matplotlib.rcParams.update({'font.size': 20})
 
 av_wide = AodVisualisation(785e-9, ac_dir_rel=[1,0,0], is_wide=True, deg_bnds=(-1,4))
 av_narrow = AodVisualisation(785e-9, ac_dir_rel=normalise([1,0,0]), is_wide=False, deg_bnds=(-1,6))
@@ -32,32 +36,36 @@ def plot_eff_ang_narrow():
     plt.plot(d.angle_narrow, d.eff_angle_narrow, 'o')
     av_narrow.plot_efficiency_xangle()
 
-def plot_eff_freq_narrow_model():
-    av_narrow_920.plot_efficiency_freq_max()
-    av_narrow_800.plot_efficiency_freq_max()
-
-def plot_eff_freq_narrow_expt():
+def plot_eff_freq_narrow_expt_model():
     plt.plot(d.freq_narrow_new, d.eff_freq_narrow_920_1, 'bo')
     plt.plot(d.freq_narrow_new, d.eff_freq_narrow_800_1, 'go')
     plt.plot(d.freq_narrow_new, d.eff_freq_narrow_920_2, 'ro')
     plt.plot(d.freq_narrow_new, d.eff_freq_narrow_800_23, 'co')
 
-def add_legend():
+    av_narrow_920.plot_efficiency_freq_max()
+    av_narrow_800.plot_efficiency_freq_max()
+    av_narrow_920.plot_efficiency_freq_max_second_order()
+    av_narrow_800.plot_efficiency_freq_max_second_order()
+
+    from aol_model.set_up_utils import transducer_efficiency_narrow
+    f = np.linspace(20, 50, 300) * 1e6
+    plt.plot(d.freq_narrow_new, transducer_efficiency_narrow(np.array(d.freq_narrow_new)*1e6), 'mo')
+    plt.plot(f/1e6, transducer_efficiency_narrow(f))
     label_list = ['920nm -1 mode expt', '800nm -1 mode expt', '920nm -2 mode expt', '800nm -2 mode expt', \
                   '920nm -1 mode model', '800nm -1 mode model', '920nm -2 mode model', '800nm -2 mode model', \
-                  'RF to acoustic']
-    plt.legend(label_list, loc=0, borderaxespad=0.8)
+                  'RF to acoustic inferred', 'RF to acoustic spline']
+    plt.legend(label_list, loc=0, borderaxespad=1.2, fontsize=16)
 
 if __name__ == '__main__':
-    #plot_eff_freq_narrow_expt()
-    #plot_eff_freq_narrow_model()
-    #add_legend()
     #plt.figure()
-    plot_eff_freq_narrow()
+    #plot_eff_freq_narrow_expt_model()
+
     #plt.figure()
-    plot_eff_freq_wide()
+    #plot_eff_freq_narrow()
     #plt.figure()
-    #plot_eff_ang_narrow()
+    #plot_eff_freq_wide()
+    plt.figure()
+    plot_eff_ang_narrow()
     #plt.figure()
     plot_eff_ang_wide()
     #plt.figure()
--- aol_modelling/pdr_image_processing/compare_pdr.py
+++ aol_modelling/pdr_image_processing/compare_pdr.py
@@ -1,11 +1,11 @@
 from __future__ import division
 import numpy as np
 
-location = r'C:\Users\Geoff\Python\python_aol_work\aol_modelling\pdr_image_processing\images\pdr%s_expt.npy'
+location = r'C:\Users\Geoff\Python\python_aol_work\aol_modelling\pdr_image_processing\images\pdr%s_model.npy'
 expt_1 = np.load(location % 1)
-expt_2 = np.load(location % 0.5)
+expt_2 = np.load(location % None)
 
-for val in map(lambda x: x/10, range(3,8)):
+for val in map(lambda x: x/10, range(2,9)):
     count1 = np.where(expt_1 > val)[0].size
     count2 = np.where(expt_2 > val)[0].size
     improvement = count2 / count1 - 1
